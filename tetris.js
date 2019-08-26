2a828e1fa70fd971d9399d8fe9b59ec6266009e9
Gavin <win7100123@gmail.com>
2019-08-26 23:17:51 +0300

    Implemented shapes

--- tetris.js
+++ tetris.js
@@ -7,21 +7,75 @@ const wpadding = 75;
 const hpadding = 20;
 const blockWidth = (width-(wpadding*2))/10;
 const blockHeight = (height-(hpadding*2))/20;
+const tetromino_colors = ["#04f7db","#ebf704","#f304f7","#0404f4", "#f77a04","#0df704","#f71904" ]
+                            //I,      O,         T,        J,          L,       S,        Z
 
 let board = [];
+let activeShape = [[0,0],[0,0],[0,0],[0,0]];
 
+const pieces = [
+	[
+		[0,1],[0,0],[0,2],[0,3] //I piece
+	],
+
+	[
+		[0,0],[1,0],[0,1],[1,1] //O piece
+	],
+	[
+		[0,0],[-1,1],[0,1],[1,1] //T piece
+	],
+	[
+		[0,1],[0,0],[-2,1],[-1,1] //J piece
+	],
+	[
+		[-1,1],[-1,0],[0,1],[1,1] //L piece
+	],
+	[
+		[0,0],[1,0],[-1,1],[0,1] //S piece
+	],
+	[
+		[0,0],[-1,0],[0,1],[1,1] //Z piece
+	]
+
+];
+//Inital State of the Shapes
+//
+//1       I
+//0
+//2
+//3
+//
+//01      O
+//23
+//
+// 0      T
+//123
+//
+//1       L
+//023
+//
+//  1     J
+//230
+//
+// 01     S
+//23
+//
+//10      Z
+// 23
+//
 context.strokeStyle = "#000000"; //Setting up the boundries visually
 context.strokeRect(0,0, width, height);
 
 function drawBlock(x, y, color){
-    //Has constant width and height
+    //Has constant width and height and may not be the same
     context.strokeStyle = "#000000";
     context.fillStyle = color;
     context.strokeRect(x, y, blockWidth, blockHeight);
     context.fillRect(x, y, blockWidth, blockHeight);
 }
 
-function initBoard(board){
+function initBoard(board){ //setting up a blank board, ready to be filled up by tetrominos
+    board = []
     for(i = 0; i<10; i++){
         board.push([]);
         for(j = 0; j<20; j++){
@@ -39,11 +93,18 @@ function drawBoard(board){
     }
 }
 
+function add_piece(board, piece){
+	let Wmiddle = Math.floor(board.length/2);
+	//let piece = 0;
+	for (i = 0; i < 4; i++){
+		let coords = [Wmiddle + pieces[piece][i][0], pieces[piece][i][1]]
+		board[coords[0]][coords[1]] = tetromino_colors[piece];
+		activeShape[i] = coords;
+	}
+}
+
 board = initBoard(board);
 function draw(){
-    frameCount++;
-    frameCount = frameCount%255;
-    console.log(frameCount);
     drawBoard(board);
     window.requestAnimationFrame(draw);
 }
